731e2affa560ca0d8532dd42157bcaf8407f806a
Juan Alvarado <juandjesus.2001@gmail.com>
2024-07-08 20:11:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio2_2/Interfaces/UsuariosApi.java
@@ -0,0 +1,22 @@
+package com.example.ejercicio2_2.Interfaces;
+
+import com.example.ejercicio2_2.Models.Usuarios;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+
+public interface UsuariosApi
+{
+    String Ruta0 = "/posts";
+    String Ruta1= "/posts/{valor}";
+
+    @GET(Ruta0)
+    Call<List<Usuarios>> getUsuarios();
+
+    @GET(Ruta1)
+    Call<Usuarios> getUsuario(@Path("valor") String valor);
+}
